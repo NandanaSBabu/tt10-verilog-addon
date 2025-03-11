eac4f40a1edc6175da9ec760d5d76b76debf235f
NandanaSBabu <133081714+NandanaSBabu@users.noreply.github.com>
2025-03-11 20:26:16 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,14 +1,14 @@
 module tt_um_project (
-    input  wire [7:0] ui,    // 8-bit input x
-    inout  wire [7:0] uio,   // 8-bit bidirectional y
-    output wire [7:0] uo,    // 8-bit output (r)
-    input  wire clk,         // Clock signal
-    input  wire rst_n,       // Active-low reset
-    input  wire ena          // Enable signal
+    input  wire [7:0] ui_in,     // 8-bit input x
+    inout  wire [7:0] uio_inout, // 8-bit bidirectional y
+    output wire [7:0] uo_out,    // 8-bit output (r)
+    input  wire clk,             // Clock signal
+    input  wire rst_n,           // Active-low reset
+    input  wire ena              // Enable signal
 );
 
-    wire [7:0] x = ui;  // x comes from input
-    wire [7:0] y = uio; // y comes from bidirectional pins
+    wire [7:0] x = ui_in;   // x is taken from input
+    wire [7:0] y = uio_inout; // y is bidirectional
 
     reg [7:0] r_reg;
 
@@ -21,6 +21,6 @@ module tt_um_project (
         end
     end
 
-    assign uo = r_reg; // Output register
+    assign uo_out = r_reg; // Output assignment
 
 endmodule
