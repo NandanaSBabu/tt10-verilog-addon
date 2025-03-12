5d30e526f16c955d1489139fc721a48318a26586
NandanaSBabu <133081714+NandanaSBabu@users.noreply.github.com>
2025-03-12 14:15:07 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,32 +1,24 @@
-`default_nettype none
-`timescale 1ns / 1ps
+# SPDX-License-Identifier: Apache-2.0
 
-module tb ();
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
 
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+@cocotb.test()
+async def test_project(dut):
+    dut._log.info("Start")
 
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
 
-  tt_um_rect_to_cyl user_project (
-      .ui_in  (ui_in),   
-      .uo_out (uo_out),  
-      .uio_in (uio_in),  
-      .uio_out(uio_out),  
-      .uio_oe (uio_oe),  
-      .ena    (ena),     
-      .clk    (clk),     
-      .rst_n  (rst_n)    
-  );
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
 
-endmodule
+    dut.ui_in.value = 0b00010010  # Example: x = 2, y = 1
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0b01010100  # Approximate expected output
