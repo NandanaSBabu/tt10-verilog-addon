2678d1841b762b6482ed236147346914d382ea03
NandanaSBabu <133081714+NandanaSBabu@users.noreply.github.com>
2025-03-20 16:07:04 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,38 +1,50 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
 module tb ();
 
-  // Dump signals to VCD for waveform analysis
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Testbench signals
+  // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-
+  reg ena;
+  reg [7:0] ui_in;   // x input
+  reg [7:0] uio_in;  // y input
+  wire [7:0] uo_out; // sqrt_out output
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+  
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
-  // Instantiate the sqrt_pythagoras module
+  // Instantiate the module under test
   tt_um_addon user_project (
+  
+      // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-    .ui_in      (ui_in),       // Input x
-    .uio_in      (uio_in),       // Input y
-      .clk    (clk),     // Clock
-      .rst_n  (rst_n),   // Reset (active low)
-    .uo_out (ou_out) // Output sqrt(x^2 + y^2)
+
+      .ui_in  (ui_in),    // x input
+      .uio_in (uio_in),   // y input
+      .uo_out (uo_out),   // sqrt_out output
+      .uio_out(uio_out),  // IOs: Output path (unused)
+      .uio_oe (uio_oe),   // IOs: Enable path (unused)
+      .ena    (ena),      // enable - always 1
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // active-low reset
   );
 
 endmodule
