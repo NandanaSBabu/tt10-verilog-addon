063b44e4dde12a589fac7fe6faa4eaf7e77e2b57
NandanaSBabu <133081714+NandanaSBabu@users.noreply.github.com>
2025-03-24 13:34:57 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,15 +7,15 @@ module tt_um_addon (
     input  wire [7:0] ui_in,
     input  wire [7:0] uio_in,
     output reg  [7:0] uo_out,
-    output wire [7:0] uio_out,  
-    output wire [7:0] uio_oe,   
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
     input  wire       ena,
     input  wire       clk,
     input  wire       rst_n
 );
 
-    assign uio_out = 8'b0; 
-    assign uio_oe  = 8'b0; 
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
     reg [15:0] sum_squares;
     reg [7:0] sqrt_temp;
@@ -35,7 +35,9 @@ module tt_um_addon (
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            uo_out <= 8'd0;
+            uo_out      <= 8'd0;
+            sum_squares <= 16'd0; // Initialize sum_squares
+            sqrt_temp   <= 8'd0;  // Initialize sqrt_temp
         end else if (ena) begin
             sum_squares = mul_shift_add(ui_in, ui_in) + mul_shift_add(uio_in, uio_in);
 
@@ -51,7 +53,7 @@ module tt_um_addon (
                 end
                 sqrt_temp = r[7:0];  // Take only lower 8 bits
             end
-            
+
             uo_out <= sqrt_temp;
         end else begin
             uo_out <= uo_out; // Hold the last value when `ena` is low
